a2bc27548d50916a5da176e1e8da94fee641aced
yunzhen-kim <kyj886811@gmail.com>
2025-07-24 16:18:32 +0900

    dvice driver mocking test

--- device_driver.h
+++ device_driver.h
@@ -1,13 +1,28 @@
 #pragma once
 #include "flash_memory_device.h"
+#include <exception>
+
+class ReadFailException : public std::exception {
+public:
+    char const* what() const {
+        return "Read failed because read data is not same 5 times";
+    }
+};
+
+class WriteFailException : public std::exception {
+public:
+    char const* what() const {
+        return "Write failed because it has valid data";
+    }
+};
 
 class DeviceDriver
 {
 public:
-    DeviceDriver(FlashMemoryDevice *hardware);
+    DeviceDriver(FlashMemoryDevice* hardware);
     int read(long address);
     void write(long address, int data);
 
 protected:
-    FlashMemoryDevice *m_hardware;
+    FlashMemoryDevice* m_hardware;
 };
--- main.cpp
+++ main.cpp
@@ -1,13 +1,4 @@
 #include "gmock/gmock.h"
-#include "device_driver.h"
-
-TEST(DeviceDriver, ReadFromHW) {
-	// TODO : replace hardware with a Test Double
-	FlashMemoryDevice* hardware = nullptr;
-	DeviceDriver driver{ hardware };
-	int data = driver.read(0xFF);
-	EXPECT_EQ(0, data);
-}
 
 int main() {
 	::testing::InitGoogleMock();
